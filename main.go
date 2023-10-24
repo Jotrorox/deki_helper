2546b3b1e148e9a92902936d2a0f4a5fec029357
Johannes (Jotrorox) <jotrorox@gmail.com>
2023-10-24 19:04:43 +0200

    Fixing Bug #5

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	twitch "github.com/gempir/go-twitch-irc/v4"
 	"github.com/glebarez/sqlite"
@@ -43,24 +44,43 @@ func createConfigFile(filename string) error {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Database path (commands.db): ")
 	db_path, _ := reader.ReadString('\n')
+	db_path = strings.Replace(db_path, "\r\n", "", -1)
+	db_path = strings.Replace(db_path, "\n", "", -1)
+
 	fmt.Print("Your Bot Name: ")
 	bot_id, _ := reader.ReadString('\n')
+	bot_id = strings.Replace(bot_id, "\r\n", "", -1)
+	bot_id = strings.Replace(bot_id, "\n", "", -1)
+
 	fmt.Print("Your OAuth Token: ")
 	bot_token, _ := reader.ReadString('\n')
 	bot_token = "oauth:" + bot_token
+	bot_token = strings.Replace(bot_token, "\r\n", "", -1)
+	bot_token = strings.Replace(bot_token, "\n", "", -1)
+
 	fmt.Print("The Channel: ")
 	channel, _ := reader.ReadString('\n')
+	channel = strings.Replace(channel, "\r\n", "", -1)
+	channel = strings.Replace(channel, "\n", "", -1)
+
 	fmt.Print("A User able to add commands: ")
 	cmd_au, _ := reader.ReadString('\n')
+	cmd_au = strings.Replace(cmd_au, "\r\n", "", -1)
+	cmd_au = strings.Replace(cmd_au, "\n", "", -1)
+
 	fmt.Print("Mention the User after a command (y/n): ")
 	mu1, _ := reader.ReadString('\n')
+	mu1 = strings.Replace(mu1, "\r\n", "", -1)
+	mu1 = strings.Replace(mu1, "\n", "", -1)
 	mu2 := false
 	if mu1 == "y" {
 		mu2 = true
 	}
+
 	var cmd_au_arr []string
 	cmd_au_arr = append(cmd_au_arr, cmd_au)
 	cmd_au_arr = append(cmd_au_arr, channel)
+
 	config := &Config{
 		DB_PATH:      db_path,
 		BOT_ID:       bot_id,
