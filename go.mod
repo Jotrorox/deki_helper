5619b9fa0e2dff07bed6df3eade6ec1c5af50174
Johannes (Jotrorox) <jotrorox@gmail.com>
2023-10-24 16:29:54 +0200

    Updating the configuration

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.21.3
 require (
 	github.com/gempir/go-twitch-irc/v4 v4.0.0
 	github.com/joho/godotenv v1.5.1
+	github.com/pelletier/go-toml v1.9.5
 )
--- main.go
+++ main.go
@@ -1,19 +1,76 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 
 	twitch "github.com/gempir/go-twitch-irc/v4"
-	dotenv "github.com/joho/godotenv"
+	toml "github.com/pelletier/go-toml"
 )
 
-func main() {
-	err := dotenv.Load()
+type Config struct {
+	DB_PATH   string
+	BOT_ID    string
+	BOT_TOKEN string
+	CHANNEL   string
+}
+
+func readConfigFile(filename string) (*Config, error) {
+	data, err := os.ReadFile(filename)
 	if err != nil {
-		panic(err)
+		return nil, err
+	}
+	config := &Config{}
+	err = toml.Unmarshal(data, config)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+func createConfigFile(filename string) error {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Database path (commands.db): ")
+	db_path, _ := reader.ReadString('\n')
+	fmt.Print("Your Bot ID: ")
+	bot_id, _ := reader.ReadString('\n')
+	fmt.Print("Your OAuth Token: ")
+	bot_token, _ := reader.ReadString('\n')
+	fmt.Print("The Channel: ")
+	channel, _ := reader.ReadString('\n')
+	config := &Config{
+		DB_PATH:   db_path,
+		BOT_ID:    bot_id,
+		BOT_TOKEN: bot_token,
+		CHANNEL:   channel,
+	}
+	data, err := toml.Marshal(config)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(filename, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	cfg_filename := "config.toml"
+
+	_, err := os.Stat(cfg_filename)
+	if os.IsNotExist(err) {
+		err = createConfigFile(cfg_filename)
+		if err != nil {
+			fmt.Println("Failed to create config file:", err)
+			return
+		}
 	}
 
-	client := twitch.NewClient(os.Getenv("BOT_NAME"), os.Getenv("OAUTH_TOKEN"))
+	cfg, _ := readConfigFile(cfg_filename)
+
+	client := twitch.NewClient(cfg.BOT_ID, cfg.BOT_TOKEN)
 
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
 		if message.Message == "!help" {
@@ -33,7 +90,7 @@ func main() {
 		}
 	})
 
-	client.Join("deki_senpai_tm")
+	client.Join(cfg.CHANNEL)
 
 	err = client.Connect()
 	if err != nil {
